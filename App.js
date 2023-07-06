d2306868059afacc221f98d318ac2e0013574f9d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-06 14:59:58 -0600

    Fix BottomTabs import

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { BottomTabs, MyTabs } from "./BottomTabs";
+import { BottomTabs } from "./BottomTabs";
 
 const App = () => {
 	return (
