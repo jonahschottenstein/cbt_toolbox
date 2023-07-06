3ef08d03a392214c244f9e1bd9ef13e67bac9891
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-05 20:05:26 -0600

    Add ToolboxEditor component

--- ToolboxEditor.js
+++ ToolboxEditor.js
@@ -1,1 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
 
+export const ToolboxEditor = () => {
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Settings!</Text>
+		</View>
+	);
+};
