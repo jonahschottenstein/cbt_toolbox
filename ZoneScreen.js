eda882da8e86493849ac9ee125deb025f1b59186
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-06 12:47:53 -0600

    Fix ZoneScreen to access props passed to it

--- ZoneScreen.js
+++ ZoneScreen.js
@@ -1,9 +1,10 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
-export const ZoneScreen = ({ zone }) => {
+export const ZoneScreen = ({ route: { params } }) => {
+	console.log(params);
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>{zone}</Text>
+			<Text>{Object.values(params)[0]}</Text>
 		</View>
 	);
 };
