e518863c47f58ef85a1130be8ef412ceed159900
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-05 20:08:44 -0600

    Add BottomTabs to App component

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { BottomTabs, MyTabs } from "./BottomTabs";
 
-export default function App() {
+const App = () => {
 	return (
 		<NavigationContainer>
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
+			<BottomTabs />
 		</NavigationContainer>
 	);
-}
+};
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+export default App;
