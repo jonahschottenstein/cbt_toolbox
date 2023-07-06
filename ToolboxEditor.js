8c7247baa374da60060ee2548ff22a54ee07a178
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-05 20:13:48 -0600

    Change text from 'Settings' to 'Toolbox Editor'

--- ToolboxEditor.js
+++ ToolboxEditor.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export const ToolboxEditor = () => {
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>Settings!</Text>
+			<Text>Toolbox Editor</Text>
 		</View>
 	);
 };
