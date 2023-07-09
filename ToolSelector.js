fbeb764c930dc5dd2cabebc565b2f8356c997b13
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-08 19:15:47 -0600

    Add state var & add onPress w/ dispatch to buttons

--- ToolSelector.js
+++ ToolSelector.js
@@ -1,16 +1,52 @@
 import { Text, View } from "react-native";
 import { SegmentedButtons } from "react-native-paper";
+import { useToolsDispatch } from "./ToolsContext";
+import { useState } from "react";
 
-export const ToolSelector = ({ value }) => {
+export const ToolSelector = ({ zone, toolIndex }) => {
+	const [value, setValue] = useState("");
+	const dispatch = useToolsDispatch();
+
 	return (
 		<View>
 			<Text>Select tool:</Text>
 			<SegmentedButtons
 				value={value}
+				onValueChange={setValue}
 				buttons={[
-					{ value: "breathing", label: "Breathing" },
-					{ value: "instruction", label: "Instruction" },
-					{ value: "video", label: "Video" },
+					{
+						value: "breathing",
+						label: "Breathing",
+						onPress: () =>
+							dispatch({
+								type: "changed_tool_type",
+								zone: zone,
+								toolIndex: toolIndex,
+								nextToolType: "breathing",
+							}),
+					},
+					{
+						value: "instruction",
+						label: "Instruction",
+						onPress: () =>
+							dispatch({
+								type: "changed_tool_type",
+								zone: zone,
+								toolIndex: toolIndex,
+								nextToolType: "instruction",
+							}),
+					},
+					{
+						value: "video",
+						label: "Video",
+						onPress: () =>
+							dispatch({
+								type: "changed_tool_type",
+								zone: zone,
+								toolIndex: toolIndex,
+								nextToolType: "video",
+							}),
+					},
 				]}
 			/>
 		</View>
