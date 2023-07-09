ecddaf1710efd7c895fb02dbbba7b9a10fb3875f
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-08 19:12:29 -0600

    Add ToolAccordionsGroup to ZoneScreen

--- ZoneScreen.js
+++ ZoneScreen.js
@@ -1,10 +1,29 @@
 import { Text, View } from "react-native";
+import { ToolAccordionsGroup } from "./ToolAccordionsGroup";
+import { useTools } from "./ToolsContext";
+import { ToolAccordion } from "./ToolAccordion";
 
 export const ZoneScreen = ({ route: { params } }) => {
-	console.log(params);
+	const zone = params.zone;
+	const tools = useTools();
+	const zoneTools = tools[zone];
+
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>{Object.values(params)[0]}</Text>
+		<View style={{ flex: 1, justifyContent: "center" }}>
+			<Text>{zone}</Text>
+			<ToolAccordionsGroup>
+				{zoneTools.map((tool, index) => (
+					<ToolAccordion
+						key={index}
+						// Remember to change key value
+						accordionProps={{
+							title: tool.type || "Select tool",
+							id: tool.index,
+						}}
+						itemProps={{ zone, toolType: tool.type, toolIndex: tool.index }}
+					/>
+				))}
+			</ToolAccordionsGroup>
 		</View>
 	);
 };
