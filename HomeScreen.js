9dfe2fdbd58112cf4e9f1d72c3ee1321047a6c17
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-10 17:27:09 -0600

    Add ToolsStack & ZoneButtons, conditionally

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import { ZoneButtons } from "./ZoneButtons";
+import { useState } from "react";
+import { useTools } from "./ToolsContext";
+import { ToolsStack } from "./ToolsStack";
 
 export const HomeScreen = () => {
+	const tools = useTools();
+
+	const [currentZone, setCurrentZone] = useState("");
+
+	const handleZonePress = (zone) => {
+		setCurrentZone(zone);
+	};
+
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>Home!</Text>
+		<View style={{ flex: 1, justifyContent: "center" }}>
+			{tools && currentZone ? (
+				<ToolsStack tools={tools[currentZone]} />
+			) : (
+				<ZoneButtons onPress={handleZonePress} />
+			)}
 		</View>
 	);
 };
