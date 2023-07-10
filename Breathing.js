cb63b160a248fe1ba65d773b487081a7f4f5633d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-10 17:10:38 -0600

    Add Breathing component

---
+++ Breathing.js
@@ -0,0 +1,85 @@
+import { useEffect, useRef, useState } from "react";
+import { BreathingDisplay } from "./BreathingDisplay";
+
+export const Breathing = ({
+	route: {
+		params: { breathingData },
+	},
+}) => {
+	// Still need to factor in sets
+	const [isRunning, setIsRunning] = useState(false);
+	const [command, setCommand] = useState("inhale");
+	const [startTime, setStartTime] = useState(null);
+	const [now, setNow] = useState(null);
+	const intervalRef = useRef(null);
+	const secondsPassedRef = useRef(breathingData.inhale);
+
+	const currentCommandSeconds =
+		breathingData && command && breathingData[command];
+
+	useEffect(() => {
+		if (startTime != null && now != null) {
+			secondsPassedRef.current =
+				currentCommandSeconds - (now - startTime) / 1000;
+		}
+		if (secondsPassedRef.current < 1.01) {
+			setStartTime(Date.now());
+			setNow(Date.now());
+			handleCommand();
+			clearInterval(intervalRef.current);
+			intervalRef.current = setInterval(() => {
+				setNow(Date.now());
+			}, 10);
+		}
+	}, [now, startTime, currentCommandSeconds]);
+
+	function handleStart() {
+		console.log("HANDLE START");
+		setStartTime(Date.now());
+		setNow(Date.now());
+		setIsRunning((prevState) => (prevState === false ? true : false));
+
+		if (!isRunning && command === "") {
+			setCommand("inhale");
+		}
+
+		clearInterval(intervalRef.current);
+		intervalRef.current = setInterval(() => {
+			setNow(Date.now());
+		}, 10);
+	}
+
+	function handleReset() {
+		clearInterval(intervalRef.current);
+		secondsPassedRef.current = breathingData["inhale"];
+		setStartTime(null);
+		setNow(null);
+		setIsRunning(false);
+		setCommand("inhale");
+	}
+
+	function handleCommand() {
+		// Can use switch statement
+		setCommand((prevState) => {
+			if (prevState === "inhale") {
+				return "hold";
+			} else if (prevState === "hold") {
+				return "exhale";
+			} else if (prevState === "exhale") {
+				return "rest";
+			} else {
+				return "inhale";
+			}
+		});
+	}
+
+	return (
+		<BreathingDisplay
+			isRunning={isRunning}
+			secondsPassed={secondsPassedRef.current}
+			command={command}
+			onStartClick={handleStart}
+			onResetClick={handleReset}
+		/>
+	);
+};
