6a616e406009025fd59494b99cc5dea638394877
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-09 23:39:36 -0600

    Update value to be from toolsReducer

--- BreathingInputsGroup.js
+++ BreathingInputsGroup.js
@@ -1,16 +1,18 @@
 import { BreathingInput } from "./BreathingInput";
+import { useTools } from "./ToolsContext";
 
 export const BreathingInputsGroup = ({ zone, toolIndex }) => {
+	const tools = useTools();
+	const zoneTools = tools[zone];
 	const labels = ["Inhale", "Hold", "Exhale", "Rest", "Sets"];
 
 	return labels.map((label, index) => (
 		<BreathingInput
 			key={label}
 			label={label}
-			value={5}
+			value={zoneTools[toolIndex]["value"][label.toLowerCase()]}
 			zone={zone}
 			toolIndex={toolIndex}
-			onChange={() => console.log("ONCHANGE")}
 		/>
 	));
 };
