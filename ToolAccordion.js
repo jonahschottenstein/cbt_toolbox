5a6b96f53e95e98973e9451fbfd7a29f672b30ae
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-11 14:28:54 -0600

    Add Divider to ToolAccordion

--- ToolAccordion.js
+++ ToolAccordion.js
@@ -1,5 +1,5 @@
 import { Text } from "react-native";
-import { List, Button } from "react-native-paper";
+import { List, Button, Divider } from "react-native-paper";
 import { BreathingInputsGroup } from "./BreathingInputsGroup";
 import { ToolSelector } from "./ToolSelector";
 import { InstructionInputsGroup } from "./InstructionInputsGroup";
@@ -9,47 +9,53 @@ export const ToolAccordion = ({ accordionProps, itemProps }) => {
 	const dispatch = useToolsDispatch();
 
 	return (
-		<List.Accordion title={accordionProps.title} id={accordionProps.id}>
-			<List.Item
-				style={{ width: "100%" }}
-				titleStyle={{ width: "100%" }}
-				title={
-					<ToolSelector zone={itemProps.zone} toolIndex={itemProps.toolIndex} />
-				}
-			/>
-			<List.Item
-				title={() =>
-					itemProps.toolType === "breathing" ? (
-						<BreathingInputsGroup
+		<>
+			<List.Accordion title={accordionProps.title} id={accordionProps.id}>
+				<List.Item
+					style={{ width: "100%" }}
+					titleStyle={{ width: "100%" }}
+					title={
+						<ToolSelector
 							zone={itemProps.zone}
 							toolIndex={itemProps.toolIndex}
 						/>
-					) : itemProps.toolType === "instruction" ? (
-						<InstructionInputsGroup
-							zone={itemProps.zone}
-							toolIndex={itemProps.toolIndex}
-						/>
-					) : (
-						<Text>Select tool</Text>
-					)
-				}
-			/>
-			<List.Item
-				title={() => (
-					<Button
-						mode="contained-tonal"
-						icon="delete"
-						onPress={() =>
-							dispatch({
-								type: "deleted",
-								zone: itemProps.zone,
-								toolIndex: itemProps.toolIndex,
-							})
-						}>
-						Delete
-					</Button>
-				)}
-			/>
-		</List.Accordion>
+					}
+				/>
+				<List.Item
+					title={() =>
+						itemProps.toolType === "breathing" ? (
+							<BreathingInputsGroup
+								zone={itemProps.zone}
+								toolIndex={itemProps.toolIndex}
+							/>
+						) : itemProps.toolType === "instruction" ? (
+							<InstructionInputsGroup
+								zone={itemProps.zone}
+								toolIndex={itemProps.toolIndex}
+							/>
+						) : (
+							<Text>Select tool</Text>
+						)
+					}
+				/>
+				<List.Item
+					title={() => (
+						<Button
+							mode="contained-tonal"
+							icon="delete"
+							onPress={() =>
+								dispatch({
+									type: "deleted",
+									zone: itemProps.zone,
+									toolIndex: itemProps.toolIndex,
+								})
+							}>
+							Delete
+						</Button>
+					)}
+				/>
+			</List.Accordion>
+			<Divider />
+		</>
 	);
 };
