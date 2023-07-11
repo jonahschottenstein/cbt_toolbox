94910ed08508b4f40b608f868fee9398aca81f45
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-10 20:33:35 -0600

    Add getSameTypeTools function

--- utilities.js
+++ utilities.js
@@ -1,2 +1,8 @@
 export const capitalize = (text) =>
 	text.slice(0, 1).toUpperCase() + text.slice(1);
+
+export const getSameTypeTools = (currentTool, tools) => {
+	const sameTypeTools = tools.filter((tool) => tool.type === currentTool.type);
+
+	return sameTypeTools;
+};
