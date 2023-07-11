183d74b3d7bad0cd908bc2b0d7d85478c474e077
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-10 20:38:56 -0600

    Update Stack.Screen props

--- ToolsStack.js
+++ ToolsStack.js
@@ -2,10 +2,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Text } from "react-native";
 import { Breathing } from "./Breathing";
 import { Instruction } from "./Instruction";
+import { getSameTypeTools, getToolTypeIndex } from "./utilities";
 
 const Stack = createStackNavigator();
 
 export const ToolsStack = ({ tools }) => {
+	// Need a way to get back to ZoneButtons Screen
+	// Not sure if I need to make it the first Screen in the Stack or if I should do it another way
+
 	const getComponent = (tool) => {
 		const component =
 			tool.type === "breathing" ? (
@@ -19,20 +23,27 @@ export const ToolsStack = ({ tools }) => {
 		return component;
 	};
 
-	const getToolValue = (tool) => tool?.value || null;
 	// if no tools, AddTool
 
+	const getNextTool = (currentToolIndex, tools) => tools[currentToolIndex + 1];
+
 	return tools.length > 0 && tools[0]["type"] !== "" ? (
-		<Stack.Navigator
-			screenOptions={{
-				headerStyle: { borderWidth: 1, flex: 1 },
-			}}>
+		<Stack.Navigator>
 			{tools.map((tool) => (
 				<Stack.Screen
 					key={tool.index}
-					name={tool.type}
+					name={
+						tool.type.slice(0, 1).toUpperCase() +
+						tool.type.slice(1) +
+						"-" +
+						getToolTypeIndex(tool, getSameTypeTools(tool, tools))
+					}
 					component={getComponent(tool)}
-					initialParams={{ toolValue: getToolValue(tool) }}
+					initialParams={{
+						tool: tool,
+						nextTool: getNextTool(tool.index, tools),
+						tools: tools,
+					}}
 				/>
 			))}
 		</Stack.Navigator>
