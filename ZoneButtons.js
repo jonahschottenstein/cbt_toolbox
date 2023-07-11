6030cf963e270df7b451a22d218fe27910c19db6
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-11 13:49:38 -0600

    Refactor by using ZoneButton components

--- ZoneButtons.js
+++ ZoneButtons.js
@@ -1,26 +1,11 @@
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+import { ZoneButton } from "./ZoneButton";
 
-export const ZoneButtons = ({ onPress }) => {
+export const ZoneButtons = ({ navigation }) => {
 	return (
 		<View style={{ alignItems: "center" }}>
 			<View>
-				<Button
-					style={{
-						borderColor: "black",
-						borderRadius: "50%",
-						borderWidth: 1,
-						height: 100,
-						width: 100,
-					}}
-					contentStyle={{
-						width: "100%",
-						height: "100%",
-					}}
-					mode="elevated"
-					buttonColor="red"
-					zone={"red"}
-					onPress={() => onPress("red")}></Button>
+				<ZoneButton navigation={navigation} zone={"red"} />
 			</View>
 			<View
 				style={{
@@ -28,38 +13,8 @@ export const ZoneButtons = ({ onPress }) => {
 					gap: 10,
 					justifyContent: "space-between",
 				}}>
-				<Button
-					style={{
-						borderColor: "black",
-						borderRadius: "50%",
-						borderWidth: 1,
-						height: 100,
-						width: 100,
-					}}
-					contentStyle={{
-						width: "100%",
-						height: "100%",
-					}}
-					mode="elevated"
-					buttonColor="yellow"
-					onPress={() => onPress("yellow")}
-				/>
-				<Button
-					style={{
-						borderColor: "black",
-						borderRadius: "50%",
-						borderWidth: 1,
-						height: 100,
-						width: 100,
-					}}
-					contentStyle={{
-						width: "100%",
-						height: "100%",
-					}}
-					mode="elevated"
-					buttonColor="blue"
-					onPress={() => onPress("blue")}
-				/>
+				<ZoneButton navigation={navigation} zone={"yellow"} />
+				<ZoneButton navigation={navigation} zone={"blue"} />
 			</View>
 		</View>
 	);
