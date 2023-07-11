2370647e071e77e0846986b8252882833d3abca8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-07-11 13:38:07 -0600

    Add ZoneButton component

---
+++ ZoneButton.js
@@ -0,0 +1,29 @@
+export const ZoneButton = ({ navigation, zone }) => {
+	const tools = useTools();
+	const onPress = useCurrentZoneSetter();
+
+	return (
+		<Button
+			style={{
+				borderColor: "black",
+				borderRadius: "50%",
+				borderWidth: 1,
+				height: 100,
+				width: 100,
+			}}
+			contentStyle={{
+				width: "100%",
+				height: "100%",
+			}}
+			mode="elevated"
+			buttonColor={zone}
+			onPressIn={() => onPress(zone)}
+			onPressOut={() =>
+				tools[zone].filter((tool) => tool.type).length > 0 &&
+				navigation.navigate(
+					capitalize(tools[zone][0]["type"]) + "-" + tools[zone][0]["index"]
+				)
+			}
+		/>
+	);
+};
