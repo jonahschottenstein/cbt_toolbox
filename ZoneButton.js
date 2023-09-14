eedad97349cee3eb0ee5a6b96abe3bcae3ecf3ad
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-09-14 17:08:33 -0600

    Update ZoneButton styles for legibility

--- ZoneButton.js
+++ ZoneButton.js
@@ -13,17 +13,26 @@ export const ZoneButton = ({ navigation, zone }) => {
 		<Button
 			style={{
 				borderColor: "black",
-				borderRadius: "50%",
+				borderRadius: "100%",
 				borderWidth: 1,
-				height: 100,
-				width: 100,
+				height: 110,
+				width: 110,
 			}}
 			contentStyle={{
 				width: "100%",
 				height: "100%",
 			}}
 			mode="elevated"
-			buttonColor={zone}
+			buttonColor={
+				zone === "blue" ? "#4185c2" : zone === "yellow" ? "#efcd17" : "#df2728"
+			}
+			labelStyle={{
+				color: "black",
+				fontSize: 20,
+				fontWeight: 600,
+				marginHorizontal: 0,
+				marginVertical: 0,
+			}}
 			onPressIn={() => hasTools && onPress(zone)}
 			onPressOut={() => {
 				if (hasTools) {
@@ -36,7 +45,8 @@ export const ZoneButton = ({ navigation, zone }) => {
 						`Navigate to Toolbox Editor to add ${capitalize(zone)} Zone tools`
 					);
 				}
-			}}
-		/>
+			}}>
+			{capitalize(zone)}
+		</Button>
 	);
 };
